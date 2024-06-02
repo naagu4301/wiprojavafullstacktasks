b9b1b9a6bdbb48beb5c029adb1c1faac9ee14cfc
naagu4301 <147630535+naagu4301@users.noreply.github.com>
2024-06-02 19:44:52 +0530

    Add files via upload

---
+++ acceptstring.java
@@ -0,0 +1,12 @@
+package wipro.com.java;
+import java.util.Scanner;
+
+public class acceptstring {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		System.out.println("Welcome"+" "+s1);
+	}
+
+}
---
+++ cml.java
@@ -0,0 +1,15 @@
+package wipro.com.java;
+import java.util.Scanner;
+
+public class cml {
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+			String s1=sc.nextLine();
+			String s2=sc.nextLine();
+			System.out.println(s1+" " +"Technologies"+" "+s2);
+		}
+
+	}
+
